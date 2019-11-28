b2a6382b0c967b457654b6e8b886d9f4eedaef9d
Veronica <veronica.21.coutts@gmail.com>
2019-11-28 11:12:38 +0200

    Added withdraw interface

---
+++ blockchain/contracts/interfaces/IPenalty.sol
@@ -0,0 +1,4 @@
+// has a function called calculate penality takes in amounts, returns penalty amount and how
+// much the person can withdraw. 
+// One penity library can manage one penality type (a specific %)
+// can only be called by its one withdraw library 
--- blockchain/contracts/interfaces/IWithdraw.sol
+++ blockchain/contracts/interfaces/IWithdraw.sol
@@ -1,3 +1,50 @@
+pragma solidity 0.5.10;
+
+contract IWithdraw {
+
+    /**
+      * @notice Emitted when the withdraw function is called.
+      */
+    event withdrawCalculated(
+        address indexed _user,
+        uint256 _withdrawAmount,
+        uint256 _penaltyAmount
+    );
+
+    /**
+      * @notice Allows the pool (and user) to check if they are able to 
+      *         wtihdraw. If the withdraw contract does not allow for 
+      *         withdrawing inside of penalty, this function will return false
+      *         inside penalty.
+      * @param  _user The address of the user being checked
+      * @param  _amount The amount the user wants to withdraw
+      * @return bool If the user can withdraw (false if they cannot)
+      * @return uint256 If the user can withdraw, this is how much they will get
+      *         _amount - penalty.
+      */
+    function canWithdraw(
+        address _user,
+        uint256 _amount
+    )
+        public
+        returns(bool, uint256);
+
+    /**
+      * @notice Tells the pool how much a user can withdraw. Emits the 
+      *         withdrawCalculated event.
+      * @return uint256 The amount the user can withdraw
+      * @return uint256 The amount of penalty (0 if there is non)
+      * @dev    This function will revert if a user cannot withdraw
+      *         during the penalty. 
+      */
+    function withdraw(
+        address _user,
+        uint256 _amount
+    )
+        public 
+        returns(uint256, uint256);
+}
+
 // Withdraw function the pool can call
 
 // Store a spcific withdraw paraiter & check realtive to each user or to entire pool for parameters
