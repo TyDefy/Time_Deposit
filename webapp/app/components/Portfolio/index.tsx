862727156a4e979cdde66c140703655c7c51c0d7
Michael Yankelev <michaelyankelev@Michaels-MacBook-Air.local>
2019-12-13 22:52:44 +0200

    fix styling

--- webapp/app/components/Portfolio/index.tsx
+++ webapp/app/components/Portfolio/index.tsx
@@ -5,11 +5,11 @@
  */
 
 import React from 'react';
-import { Theme, createStyles, withStyles, WithStyles, Typography, Grid, Paper } from '@material-ui/core';
+import { Theme, createStyles, withStyles, WithStyles, Typography, Grid, Paper, Container } from '@material-ui/core';
 import { Pool } from 'containers/App';
 import PoolCardList from 'components/PoolCardList';
 
-const styles = ({spacing, palette}: Theme) =>
+const styles = ({ spacing, palette }: Theme) =>
   createStyles({
     poolGrid: {
       paddingLeft: spacing(1),
@@ -38,25 +38,32 @@ const Portfolio: React.FC<OwnProps> = ({
   interestAccrued,
   interestAvailable,
   pools,
-}: OwnProps) => <>  
+}: OwnProps) => <Container maxWidth='lg'>
     <Typography variant='h5'>Overview</Typography>
     <Typography variant='subtitle2'>Your Portfolio Summary</Typography>
     <Paper className={classes.poolPaper}>
-      <Grid container direction='column'>
+      <Grid container>
         <Grid item xs={12}>
           <Typography>Total Holdings</Typography>
           <Typography>{`${totalHoldings.toFixed(2)} DAI`}</Typography>
-          <Typography>{`${(portfolioInterestRate*100).toFixed(2)} %`}</Typography>
+          <Typography>{`${(portfolioInterestRate * 100).toFixed(2)} %`}</Typography>
+        </Grid>
+        <Grid item xs={4}>
           <Typography>Contributed</Typography>
           <Typography>{`${contributed.toFixed(2)} DAI`}</Typography>
+        </Grid>
+        <Grid item xs={4}>
           <Typography>Interest Accrued</Typography>
           <Typography>{`${interestAccrued.toFixed(2)} DAI`}</Typography>
+        </Grid>
+        <Grid item xs={4}>
           <Typography>Interest Available</Typography>
           <Typography>{`${interestAvailable.toFixed(2)} DAI`}</Typography>
         </Grid>
       </Grid>
     </Paper>
+    <br />
     <PoolCardList listLabel='Your Pools' pools={pools} />
-  </>;
+  </Container>;
 
 export default withStyles(styles, { withTheme: true })(Portfolio);
