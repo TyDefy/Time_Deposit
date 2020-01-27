64b836701cb62cb04ba00e7ea74685ddc581ddfc
Kent Fourie <kent@linumlabs.com>
2020-01-27 14:26:02 +0200

    poolfactorySaga

--- webapp/app/containers/App/poolFactorySaga.ts
+++ webapp/app/containers/App/poolFactorySaga.ts
@@ -104,10 +104,10 @@ function* deployedPoolWatcher() {
       })
     };
 
-    poolFactoryContract.on(poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null),
+    poolFactoryContract.on(poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null, null),
       poolDeployedHandler);
     return () => {
-      poolFactoryContract.off(poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null),
+      poolFactoryContract.off(poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null, null),
         poolDeployedHandler);
     };
   });
@@ -170,7 +170,7 @@ export default function* poolFactorySaga() {
   const { poolFactoryContract, provider }: BlockchainContext = yield getContext('blockchain')
 
   const deployedPoolEventFilter = {
-    ...poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null),
+    ...poolFactoryContract.filters.DeployedPool(null, null, null, null, null, null, null, null),
     fromBlock: 0,
     toBlock: 'latest',
   }
