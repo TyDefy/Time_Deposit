59ca0425dca500a44e21dab6c601f2565f206b1a
Kent Fourie <kent@linumlabs.com>
2020-01-31 14:28:54 +0200

    penalty

--- webapp/app/containers/HomePage/selectors.ts
+++ webapp/app/containers/HomePage/selectors.ts
@@ -9,28 +9,31 @@ import { selectEthAddress, selectExchangeRate, selectInterestRate } from 'contai
 
 export const selectPools = createSelector((state: RootState) => state.pools, selectEthAddress, selectExchangeRate, selectInterestRate, (pools, ethAddress, exchangeRate, interestRate) =>
   Object.values(pools).map(p => {
-    const cdaiBalance = p.transactions.reduce((poolCdaiBalance, t) =>
+
+    const cdaiBalancePool = p.transactions?.reduce((poolCdaiBalance, t) =>
     t.type === 'Deposit' ? poolCdaiBalance += t.cdaiAmount : poolCdaiBalance -= t.cdaiAmount, 0) || 0;
     
+    const cdaiByUser =  ethAddress ?
+    p.transactions?.filter(t => t.userAddress.toUpperCase() === ethAddress.toUpperCase())
+      .reduce((poolCdaiBalance, t) => t.type === 'Deposit' ? poolCdaiBalance += t.cdaiAmount : poolCdaiBalance -= t.cdaiAmount, 0) : 0;
+
     const contribution =  ethAddress ?
     p.transactions?.filter(t => t.userAddress === ethAddress)
       .reduce((contribution, t) => t.type === 'Deposit' ? contribution += t.amount : contribution -= t.amount, 0) : 0;
 
-    const cdaiByUser =  ethAddress ?
-      p.transactions?.filter(t => t.userAddress === ethAddress)
-        .reduce((cdaiByUser, t) => t.type === 'Deposit' ? cdaiByUser += t.cdaiAmount : cdaiByUser -= t.cdaiAmount, 0) : 0;
-
     const balance = p.transactions?.reduce((poolBalance, t) =>
     t.type === 'Deposit' ? poolBalance += t.amount : poolBalance -= t.amount, 0) || 0;
+    
+   // const penaltyAmount = p.userTotalBalanceAndPenaltiesCDai ? (p.userTotalBalanceAndPenaltiesCDai - cdaiByUser): 0;
 
     return {
       ...p,
       interestRate: interestRate,
       balance: balance,
-      cdaiBalance: cdaiBalance,
+      cdaiBalance: cdaiBalancePool,
       participants: new Set(p.transactions?.map(t => t.userAddress)).size,
       contribution: contribution,
-      interestAccrued: (cdaiBalance * exchangeRate) - balance,
+      interestAccrued: (cdaiByUser * exchangeRate) - contribution,
       availableInterest: p.period === 0 ? (cdaiByUser * exchangeRate) - contribution : 0
     }
   })
