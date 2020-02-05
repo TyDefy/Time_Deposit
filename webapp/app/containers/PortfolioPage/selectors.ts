b411c2d79e568b40a5b0fb1518366851b5185ba8
Michael Yankelev <michaelyankelev@Michaels-MacBook-Air.local>
2020-02-05 20:18:09 +0200

    fix leftover debugger

--- webapp/app/containers/PortfolioPage/selectors.ts
+++ webapp/app/containers/PortfolioPage/selectors.ts
@@ -2,7 +2,7 @@ import { createStructuredSelector, createSelector } from 'reselect';
 import { RootState } from 'containers/App/types';
 import { StateProps } from '.';
 import { selectPools } from 'containers/HomePage/selectors';
-import { selectExchangeRate } from 'containers/App/selectors';
+import { selectExchangeRate, selectInterestRate } from 'containers/App/selectors';
 
 const selectPortfolioPools = createSelector(selectPools,
   (allPools) => allPools.filter(p => p.contribution && p.contribution > 0))
@@ -13,11 +13,7 @@ const selectPortfolioTotalHoldings = createSelector(selectPortfolioPools, select
 
 const selectPortfolioContributed = createSelector(selectPortfolioPools,
   pools => pools.reduce((total, pool) => total += pool.contribution || 0, 0));
-
-  const selectPortfolioInterestRate = createSelector(selectPortfolioPools,
-    pools => 0)
   
-
 const selectPortfolioInterestAccrued = createSelector(selectPortfolioPools,
   pools => pools.reduce((total, pool) => total += pool.interestAccrued || 0, 0));
 
@@ -27,7 +23,7 @@ const selectPortfolioInterestAvailable = createSelector(selectPortfolioPools,
 const selectPortfolioPage = createStructuredSelector<RootState, StateProps>({
   pools: selectPortfolioPools,
   totalHoldings: selectPortfolioTotalHoldings,
-  portfolioInterestRate: selectPortfolioInterestRate,
+  portfolioInterestRate: selectInterestRate,
   contributed: selectPortfolioContributed,
   interestAccrued: selectPortfolioInterestAccrued,
   interestAvailable: selectPortfolioInterestAvailable,
